7334685b5050219851c0f430cf594af2a8a1e499
enetsos <walter.losa@supsi.ch>
2024-01-05 01:48:29 +0100

    fixed linux

--- client/Game.h
+++ client/Game.h
@@ -1,10 +1,10 @@
 #pragma once
-#include "Node.h"
+#include "node.h"
 #include <glm.hpp>
-#include <gtx/string_cast.hpp>
 #include <gtc/matrix_transform.hpp>
 #include <gtc/quaternion.hpp>
 #include <gtx/matrix_decompose.hpp>
+#include <gtx/string_cast.hpp>
 #include <math.h>
 #include <vector>
 
@@ -13,25 +13,26 @@
 
 class Game {
 public:
-    Game(std::vector<Node*> cars);
-    ~Game();
+  Game(std::vector<Node *> cars);
+  ~Game();
+
 public:
-    void moveCar(glm::vec2 direction);
-    void setMovementSpeed(float movementSpeed);
-    float getMovementSpeed();
-    void setActiveCar(int activeCar);
-    int getActiveCar();
-    void update();
+  void moveCar(glm::vec2 direction);
+  void setMovementSpeed(float movementSpeed);
+  float getMovementSpeed();
+  void setActiveCar(int activeCar);
+  int getActiveCar();
+  void update();
 
 private:
-    std::vector<Node*> cars;
-    std::vector<glm::vec3> carsPosition;
-    std::vector<glm::mat4> carsOriginalTransform;
+  std::vector<Node *> cars;
+  std::vector<glm::vec3> carsPosition;
+  std::vector<glm::mat4> carsOriginalTransform;
 
-    glm::vec3 getOrientation(const Node* node);
+  glm::vec3 getOrientation(const Node *node);
 
-    float movementSpeed = 1.0f;
-    int activeCar = 0;
+  float movementSpeed = 1.0f;
+  int activeCar = 0;
 };
 
-#endif //GAME
+#endif // GAME
