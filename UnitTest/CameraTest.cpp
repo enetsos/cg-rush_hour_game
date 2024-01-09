b99e71af6fd37bed1d84cd1eaa946289212fb6e6
enetsos <walter.losa@supsi.ch>
2024-01-09 02:11:01 +0100

    pipilene

--- UnitTest/CameraTest.cpp
+++ UnitTest/CameraTest.cpp
@@ -1,36 +1,32 @@
-#include "Test.h"
+#include "camera.h"
 #include "PerspectiveProjection.h"
-#include "Camera.h"
+#include "Test.h"
 
-Camera* c;
-Projection* p;
+Camera *c;
+Projection *p;
 
-void projectionGetterTest() {
-	assert(c->getProjection() == p);
-}
+void projectionGetterTest() { assert(c->getProjection() == p); }
 
 void inverseTest() {
 
-	glm::mat4 translation = glm::translate(
-		glm::mat4(1.0f),
-		glm::vec3(1.0f, 0.0f, 0.0f)
-	);
+  glm::mat4 translation =
+      glm::translate(glm::mat4(1.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-	int id = Object::getNextId();
-	Node* root = new Node(id, std::to_string(id));
-	root->setTransform(translation);
-	root->addChild(c);
+  int id = Object::getNextId();
+  Node *root = new Node(id, std::to_string(id));
+  root->setTransform(translation);
+  root->addChild(c);
 
-	assert(c->getInverse() == glm::inverse(translation));
+  assert(c->getInverse() == glm::inverse(translation));
 }
 
 void cameraTest() {
 
-	int id = Object::getNextId();
-	p = new PerspectiveProjection(1920, 1080);
-	c = new Camera(id, std::to_string(id), p);
+  int id = Object::getNextId();
+  p = new PerspectiveProjection(1920, 1080);
+  c = new Camera(id, std::to_string(id), p);
 
-	projectionGetterTest();
+  projectionGetterTest();
 
-	inverseTest();
+  inverseTest();
 }
